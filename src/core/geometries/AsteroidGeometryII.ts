d82afd7a702aa0d7de8c8efaaf808dc2108c1f7e
Дмитрий <45686133+j2m5@users.noreply.github.com>
2024-12-03 15:22:04 +0300

    Delete src/core/geometries/AsteroidGeometryII.ts

--- src/core/geometries/AsteroidGeometryII.ts
+++
@@ -1,7 +0,0 @@
-import { DodecahedronGeometry } from 'three'
-
-class AsteroidGeometryII extends DodecahedronGeometry {
-  public constructor(radius: number, detail: number = 0) {
-    super()
-  }
-}
